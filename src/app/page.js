d2f96c8cf2dab9dd0e29401b2e6bd66ddfb2c364
faisalEsMagico <faisal.khan@esmagico.in>
2024-06-25 12:27:24 +0530

    install ga

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import ReactGA from "react-ga";
+
+import { useEffect } from "react";
+ReactGA.initialize("UA-000000-01");
 
 export default function Home() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-    hello this is my app
-      </div>
+      <div className={styles.description}>hello this is my app</div>
     </main>
   );
 }
